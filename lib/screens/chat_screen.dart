028c189ddcaa493932eff63efb3a8fbfd3b9fd3e
Muhammad Azzam <MazzamR08@gmail.com>
2022-02-25 21:32:43 +0700

    going on

---
+++ lib/screens/chat_screen.dart
@@ -0,0 +1,85 @@
+import 'package:chat_app/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class ChatScreen extends StatefulWidget {
+  static const String id = 'CHAT_SCREEN';
+
+  const ChatScreen({Key? key}) : super(key: key);
+
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+
+  final _auth = FirebaseAuth.instance;
+  late User loggedInUser;
+
+  void getCurrentUser(){
+    try{
+      final user = _auth.currentUser;
+      if (user != null){
+        loggedInUser = user;
+        print(loggedInUser.email);
+      }
+    } catch(e){}
+  }
+
+  @override
+  void initState(){
+    getCurrentUser();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: null,
+        title: const Text('Chat'),
+        backgroundColor: Colors.lightBlueAccent,
+        actions: [
+          IconButton(
+              onPressed: () {
+                // for logout
+              },
+              icon: const Icon(Icons.close))
+        ],
+      ),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              decoration: kMessageContainerDecoration,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: TextField(
+                      onChanged: (value) {
+                        // for user input
+                      },
+                      decoration: kMessageTextFieldDecoration,
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      // for send some message
+                    },
+                    child: const Text(
+                      'Send',
+                      style: kSendButtonTextStyle,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/registration_screen.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import 'chat_screen.dart';
+
+class RegistrationScreen extends StatefulWidget {
+  static const String id = 'REGISTRATION_SCREEN';
+
+  const RegistrationScreen({Key? key}) : super(key: key);
+
+  @override
+  _RegistrationScreenState createState() => _RegistrationScreenState();
+}
+
+class _RegistrationScreenState extends State<RegistrationScreen> {
+
+  final _auth = FirebaseAuth.instance;
+
+  late String email;
+  late String password;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            SizedBox(
+              height: 200,
+              child: Image.asset('images/logo.png'),
+            ),
+            const SizedBox(
+              height: 48.0,
+            ),
+            TextField(
+              keyboardType: TextInputType.emailAddress,
+              style: const TextStyle(color: Colors.black),
+              textAlign: TextAlign.center,
+              onChanged: (value){
+                email = value;
+                // input your email
+              },
+              decoration: const InputDecoration(
+                hintText: 'Input your Email',
+                hintStyle: TextStyle(color: Colors.grey, fontSize: 15),
+                contentPadding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(32)),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                  borderSide: BorderSide(color: Colors.lightBlueAccent, width: 1)
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                  borderSide: BorderSide(color: Colors.lightBlueAccent, width: 2.0)
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 8.0,
+            ),
+            TextField(
+              obscureText: true,
+              style: const TextStyle(color: Colors.black),
+              textAlign: TextAlign.center,
+              onChanged: (value){
+                password = value;
+                // input your email
+              },
+              decoration: const InputDecoration(
+                hintText: 'Input your Password',
+                hintStyle: TextStyle(color: Colors.grey, fontSize: 15),
+                contentPadding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(32)),
+                ),
+                enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                    borderSide: BorderSide(color: Colors.lightBlueAccent, width: 1)
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                  borderSide: BorderSide(color: Colors.blueAccent, width: 2.0)
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 24.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0),
+              child: Material(
+                color: Colors.blueAccent,
+                borderRadius: const BorderRadius.all(Radius.circular(30)),
+                child: MaterialButton(
+                  onPressed: () async {
+                    try{
+                      await _auth.signInWithEmailAndPassword(email: email, password: password);
+                      Navigator.pushNamed(context, ChatScreen.id);
+                    } catch(e) {
+                      print(e);
+                    }
+                  },
+                  minWidth: 200.0,
+                  height: 42,
+                  child: const Text('Register', style: TextStyle(color: Colors.white),),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
